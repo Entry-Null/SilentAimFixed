a5d563853fba6e7290bb7e347cd4506ad2b18ca1
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2022-01-19 16:14:26 -0600

    Update pipebomb.lua

--- pipebomb.lua
+++ pipebomb.lua
@@ -86,19 +86,7 @@ local function getClosestPlayer()
         if not Character then
             continue
         end
-
-        if Player:IsFriendsWith(game.LocalPlayer.UserId) and Toggles.friendCheck then
-            break
-        end
-
-        if Player:IsInGroup(Options.groupID) then
-            break 
-        end
-
-        if Player.Team.Name == Options.teamCheckID then
-            break
-        end
-
+        
         local HumanoidRootPart = FindFirstChild(Character, "HumanoidRootPart")
         local Humanoid = FindFirstChild(Character, "Humanoid")
 
